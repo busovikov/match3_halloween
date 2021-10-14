6aff54d1d7b3c72bbba6b5945a6a3da15fb0729b
Pavel Busovikov <pavel.busovikov@gmail.com>
2021-10-14 20:14:00 +0200

    Add animation and new assets

--- Assets/Scripts/LevelLoader.cs
+++ Assets/Scripts/LevelLoader.cs
@@ -5,16 +5,27 @@ using UnityEngine.SceneManagement;
 
 public class LevelLoader : MonoBehaviour
 {
+    private Animator animator;
+    public float trnsactionTime = 1f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     public void StartQuickGame()
     {
-        SceneManager.LoadScene("Quick Game");
+         StartCoroutine(MakeTransactionToQuickGame());
         
+    }
+
+    IEnumerator MakeTransactionToQuickGame()
+    {
+        animator.SetTrigger("Out");
+
+        yield return new WaitForSeconds(trnsactionTime);
+
+        SceneManager.LoadScene("Quick Game");
     }
 
     // Update is called once per frame

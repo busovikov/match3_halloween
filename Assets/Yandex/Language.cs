638e4187d219905dd161b2951811c17d518fb266
Pavel Busovikov <pavel.busovikov@gmail.com>
2022-11-27 12:31:33 +0100

    Localization

---
+++ Assets/Yandex/Language.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Language : MonoBehaviour
+{
+    public LangObject ru;
+    public LangObject en;
+    public LangObject tr;
+    static public LangObject current;
+
+
+    public Text _menu_play_time;
+    public Text _menu_play_moves;
+    public Text _menu_play_exit;
+    public Text _menu_play_credits;
+    public Text _menu_play_back;
+    public Text _menu_play_menu;
+    public Text _menu_play_next;
+    public Text _menu_play_repeate;
+           
+    public Text _hud_score_label;
+    public Text _popup_score_label;
+    public Text _popup_best_label;
+    public Text _popup_total_label;
+    public Text _hud_level_label;
+    public Text _hud_play_time;
+    public Text _hud_play_moves;
+
+    public Text _popup_win_label;
+    public Text _popup_lose_label;
+
+    public void ChangeLang()
+    {
+#if UNITY_EDITOR
+        string lang = "ru";
+#else
+    #if UNITY_WEBGL
+         string lang = GetLang();
+    #endif
+#endif
+        if (lang == "ru")
+        {
+            current = ru;
+        }
+        else if (lang == "en")
+        {
+            current = en;
+        }
+        else if (lang == "tr")
+        {
+            current = tr;
+        }
+        else 
+        {
+            current = en;
+        }
+    }
+
+    private void UpdateText()
+    {
+        if (null != _menu_play_time) _menu_play_time.text = current._menu_play_time;
+        if (null != _menu_play_moves) _menu_play_moves.text = current._menu_play_moves;
+        if (null != _menu_play_exit) _menu_play_exit.text = current._menu_play_exit;
+        if (null != _menu_play_credits) _menu_play_credits.text = current._menu_play_credits;
+        if (null != _menu_play_back) _menu_play_back.text = current._menu_play_back;
+        if (null != _menu_play_menu) _menu_play_menu.text = current._menu_play_menu;
+        if (null != _menu_play_next) _menu_play_next.text = current._menu_play_next;
+        if (null != _menu_play_repeate) _menu_play_repeate.text = current._menu_play_repeate;
+        
+        if (null != _hud_score_label) _hud_score_label.text = current._hud_score_label;
+        if (null != _popup_score_label) _popup_score_label.text = current._popup_score_label;
+        if (null != _popup_best_label) _popup_best_label.text = current._popup_best_label;
+        if (null != _popup_total_label) _popup_total_label.text = current._popup_total_label;
+        if (null != _hud_level_label) _hud_level_label.text = current._hud_level_label;
+        if (null != _hud_play_time) _hud_play_time.text = current._hud_play_time;
+        if (null != _hud_play_moves) _hud_play_moves.text = current._hud_play_moves;
+    }
+
+    private void Awake()
+    {
+        ChangeLang();
+        UpdateText();
+    }
+
+    [DllImport("__Internal")]
+    private static extern string GetLang();
+
+}
---
+++ Assets/Yandex/Yandex.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Runtime.InteropServices;
+public class Yandex : MonoBehaviour
+{
+    [DllImport("__Internal")]
+    private static extern void Hello();
+
+}
